9c2e34b2060c1f836e2d39dba1d8fa44f2a2f0fc
Ron Rodenberg <rodenberg@gmail.com>
2015-12-14 14:15:03 -0600

    Test

--- rpg/urls.py
+++ rpg/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^logout/$', logout.get, name='logout'),
     url(r'^login/submit/$', login.post, name='authenticate'),
     url(r'api/properties/$', properties.select, name='properties'),
+    url(r'api/thingy/$', properties.select, name='thingy'),
 ]
 
